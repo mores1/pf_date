25e6ee2922095b8bf41b658ed6de9d938842b254
mores1 <sf1021938@gmail.com>
2022-04-10 09:51:58 +0000

    fix_routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#index"
 
-  resources :users, only: [:index, :show, :edit, :unsubscribe, :withdrow, :create, :update] do
+  resources :users, only: [:index, :show, :edit, :create, :update] do
     get 'unsubscribe' => 'users#unsubscribe'
     patch 'withdrow' => 'users#withdrow'
   end
 
-  resources :spots, only: [:new, :index, :show, :edit, :create, :update, :destroy] do
+  resources :spots do
     resource :spot_comments, only: [:create, :destroy]
   end
 
