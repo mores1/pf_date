fdee88ece5cf94d4f6f1637bf4f1533a4e6f28ca
mores1 <sf1021938@gmail.com>
2022-04-10 10:13:28 +0000

    add_bootstrap_fontawesome

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+// 以下bootstrapの為追記
+const webpack = require('webpack')
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: 'popper.js'
+  })
+)
