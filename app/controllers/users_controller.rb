62f1a4e0f2a64fcb3cf27da90350ea175772a66d
mores1 <sf1021938@gmail.com>
2022-04-10 09:48:33 +0000

    add controller_model_routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def unsubscribe
+  end
+
+  def withdrow
+  end
+end
---
+++ db/migrate/20220410083043_create_plans.rb
@@ -0,0 +1,12 @@
+class CreatePlans < ActiveRecord::Migration[6.1]
+  def change
+    create_table :plans do |t|
+      t.integer :user_id
+      t.integer :spot_id
+      t.string :title
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
