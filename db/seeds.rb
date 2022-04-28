3a8ac22c4c31df29c62bdccdde1eecf06b3b0084
mores1 <sf1021938@gmail.com>
2022-04-28 15:50:10 +0000

    dbtest2

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(
-  name: "admin",
-  email: "admin@example.jp",
-  password: "ffffff",
-  password_confirmation: "ffffff",
-  admin: true
-  )
-
-
-# 10.times do |n|
-#   User.create!(
-#     email: "stest#{n + 1}@test",
-#     name: "stest#{n + 1}",
-#     password: "ffffff"
+# User.create!(
+#   name: "admin",
+#   email: "admin@example.jp",
+#   password: "ffffff",
+#   password_confirmation: "ffffff",
+#   admin: true
 #   )
-# end
+
+
+10.times do |n|
+  User.create!(
+    email: "stest#{n + 1}@test",
+    name: "stest#{n + 1}",
+    password: "ffffff"
+  )
+end
